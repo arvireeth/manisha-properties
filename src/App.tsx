9d52da16d54e70ac59f96f14ed703a383fb3c050
Arvi Reeth <arvireeth@gmail.com>
2026-06-29 12:44:41 +0530

    Add Google Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -16,17 +16,18 @@ function App() {
         <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-0B88R2EHR4"
-        ></script>
+        />
 
-        <script>
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-0B88R2EHR4');
-          `}
-        </script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-0B88R2EHR4');
+            `,
+          }}
+        />
       </Helmet>
 
       <div className="min-h-screen flex flex-col">
